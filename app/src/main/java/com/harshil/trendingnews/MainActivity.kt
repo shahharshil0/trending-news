6f1768704d9a014a6b3faa34fdc8a54975559ce6
Harshil Shah <shahharshil0@yahoo.com>
2021-05-16 11:18:42 +0530

    Initial commit

---
+++ app/src/main/java/com/harshil/trendingnews/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.harshil.trendingnews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
